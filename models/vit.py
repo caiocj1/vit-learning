6282d722fd18513cbe6d7245823dec156b0089ba
caiocj1 <caioj2906@gmail.com>
2023-06-08 16:27:12 +0900

    fixed vit, implemented attn

--- models/vit.py
+++ models/vit.py
@@ -13,25 +13,47 @@ class PatchEmbedding(nn.Module):
 
         self.num_patches = (image_size // patch_size) ** 2
 
+        self.layer_norm1 = nn.LayerNorm(self.num_channels * (self.patch_size ** 2))
         self.linear = nn.Linear(self.num_channels * (self.patch_size ** 2), hidden_size)
+        self.layer_norm2 = nn.LayerNorm(hidden_size)
 
     def forward(self, imgs):
         imgs = imgs.view(-1, self.num_patches, self.num_channels * (self.patch_size ** 2))
-        return self.linear(imgs)
+        return self.layer_norm2(self.linear(self.layer_norm1(imgs)))
 
 class MultiheadAttention(nn.Module):
-    def __init__(self, hidden_size, num_heads, dropout):
+    def __init__(self, hidden_size, num_heads, dropout, head_size=64):
         super().__init__()
+        self.num_heads = num_heads
+        self.inner_dim = num_heads * head_size
+        self.scale = head_size ** (- 0.5)
 
-        self.w_q = nn.Linear(hidden_size, hidden_size)
-        self.w_k = nn.Linear(hidden_size, hidden_size)
-        self.w_v = nn.Linear(hidden_size, hidden_size)
+        self.to_qkv = nn.Linear(hidden_size, 3 * self.inner_dim, bias=False)
 
-        self.multihead_attn = nn.MultiheadAttention(hidden_size, num_heads=num_heads, dropout=dropout, batch_first=True)
+        self.attend = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
+
+        #self.multihead_attn = nn.MultiheadAttention(self.inner_dim, num_heads=num_heads, dropout=dropout, batch_first=True)
+
+        self.out = nn.Sequential(
+            nn.Linear(self.inner_dim, hidden_size),
+            nn.Dropout(dropout)
+        )
 
     def forward(self, x):
-        q, k, v = self.w_q(x), self.w_k(x), self.w_v(x)
-        return self.multihead_attn(q, k, v)
+        q, k, v = self.to_qkv(x).chunk(3, dim=-1)
+        b, n, hd = q.shape
+
+        q = q.reshape(b, n, self.num_heads, -1).transpose(1, 2)
+        k = k.reshape(b, n, self.num_heads, -1).transpose(1, 2)
+        v = v.reshape(b, n, self.num_heads, -1).transpose(1, 2)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+        attn = self.dropout(self.attend(dots))
+
+        out = torch.matmul(attn, v)
+        out.transpose(1, 2).reshape(b, n, -1)
+        return self.out(out)
 
 class TransformerBlock(nn.Module):
     def __init__(self, hidden_size, num_heads, dropout, mlp_size):
@@ -49,18 +71,21 @@ class TransformerBlock(nn.Module):
         )
 
     def forward(self, x):
-        x = x + self.multihead_attn(self.layer_norm1(x))[0]
+        x = x + self.multihead_attn(self.layer_norm1(x))
         return x + self.mlp(self.layer_norm2(x))
 
 class TransformerEncoder(nn.Module):
     def __init__(self, hidden_size, num_blocks, num_heads, dropout, mlp_size):
         super().__init__()
 
-        blocks = [TransformerBlock(hidden_size, num_heads, dropout, mlp_size)] * num_blocks
-        self.encoder = nn.Sequential(*blocks)
+        self.layers = nn.ModuleList([])
+        for i in range(num_blocks):
+            self.layers.append(TransformerBlock(hidden_size, num_heads, dropout, mlp_size))
 
     def forward(self, x):
-        return self.encoder(x)
+        for block in self.layers:
+            x = block(x)
+        return x
 
 class ViT(nn.Module):
     def __init__(self, hidden_size=768, num_blocks=12, num_heads=12, dropout=0.0, mlp_size=3072):
