b016f05495de53e9f4ec981546f7808fd22e80b8
caiocj1 <caioj2906@gmail.com>
2023-06-14 15:04:47 +0900

    bs 1024, corrected lr scheduler, removed grad clip

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ if __name__ == "__main__":
 
     train_dataset = ImageFolder("inputs/imagenet/train", transform=val_preprocess)
     train_dataloader = DataLoader(train_dataset,
-                                  batch_size=512,
+                                  batch_size=1024,
                                   num_workers=16,
                                   shuffle=True)
 
     val_dataset = ImageFolder("inputs/imagenet/val", transform=val_preprocess)
     val_dataloader = DataLoader(val_dataset,
-                                batch_size=512,
+                                batch_size=1024,
                                 num_workers=16,
                                 shuffle=False)
 
